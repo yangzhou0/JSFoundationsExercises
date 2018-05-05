b3a9c92006a3fdc0f402c7c26a7c6d00772a45ab
Yang Zhou <yzhou3991@gmail.com>
2018-05-04 19:31:54 -0700

    write inherits method to imitate object.create

---
+++ inheritance.js
@@ -0,0 +1,18 @@
+//https://github.com/appacademy/curriculum/tree/master/javascript/projects/inherits_exercises
+Function.prototype.inherits = function(func){
+  function Surrogate(){};
+  Surrogate.prototype = func.prototype;
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
+}
+
+
+/*
+How would you test Function.prototype.inherits? A few specs to consider:
+
+You should be able to define methods/attributes on MovingObject that ship and asteroid instances can use.
+Defining a method on Asteroid's prototype should not mean that a ship can call it.
+Adding to Ship or Asteroid's prototypes should not change MovingObject's prototype.
+The Ship and Asteroid prototypes should not be instances of MovingObject.
+
+*/
